746cfc7fbcfc8c0262cc06a88ae525077de65ea1
Lívia Santos <liviasantosdepaula21@gmail.com>
2024-07-31 13:28:09 -0300

    att

--- projeto001.todolist/app.js
+++ projeto001.todolist/app.js
@@ -1,14 +1,62 @@
 'use strict';
 
- const criarItem = (text, status='') => {
+let banco = [
+    {'tarefa':'Estudar JS', 'status': 'checked'}
+];
+
+ const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo_item');
-    item.innerHTML = `<label class="todo_item">
-        <input type="checkbox" ${status}>
-        <div>${text}</div>
-        <input type="button" value="X">
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice= ${indice}>
      </label>
     `
     document.getElementById('todoList').appendChild(item);
 
 }
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList')
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach( (item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        banco.push ({'tarefa': texto, 'status': ''})
+        atualizarTela();
+        evento.target.value = ''
+    }
+}
+const removerItem = (indice) => {
+    banco.splice (indice, 1);
+    atualizarTela();
+}
+const atualizarItem = (indice) => {
+    banco[indice].status = banco[indice].status === '' ? 'checked' : ''
+    atualizarTela();
+}
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    if(elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem(indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice; }
+        atualizarItem (indice);
+}
+
+document.getElementById('newItem').addEventListener('keypress',inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+atualizarTela();
